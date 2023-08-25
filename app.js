967d68f7eeb32490f4c9a48076bcc1422b88c2d3
abdulhaseebj <abdulhaseeb4326@gmail.com>
2023-08-25 10:06:43 -0700

    'practice'

---
+++ app.js
@@ -0,0 +1,110 @@
+
+// const age = 29;
+// const ages = 30
+
+// if (19>20 && 30>29) {
+//     console.log("age is 19");
+// }
+
+// else {
+//     console.log("age is not 19");
+// }
+
+// switch (age) {
+//     case 29:
+//         console.log("yor are 29");
+//         break;
+//     case 39:
+//         console.log("yor are 39");
+//         break;
+//     case 49:
+//         console.log("yor are 49");
+//         break;
+//     default:
+//         console.log("yor are unknown age");
+//         break;
+// }
+
+// let num = [1, 2, 3];
+// console.log(num);
+
+// push
+// Pop
+// unshift
+// shift
+// Splice
+// slice
+// join
+// split
+// indexOf
+// concat
+// sort
+// reverse
+
+
+
+// let fruits = ['grapes' , 'apple' , 'watermelon' , 'banana' , 'mango' , 'nashpati' , 'anar' , 'peach'];
+
+// console.log(fruits[2]);
+// fruits.pop();
+// fruits.push('khera');
+// fruits.shift();
+// fruits.unshift('banana');
+// let eaten = fruits.slice(1 );
+// fruits.splice(2 , 3 , 'honda' );
+// console.log(eaten);
+// console.log(fruits);
+
+
+
+
+
+
+// let names = 'my name is Abdul Haseeb and my age is 18';
+// let newArr = names.split(' ');
+// newArr[4] = "usman";
+// // newArr.splice(6);
+// console.log(newArr);
+// console.log(names);
+// let newStr = newArr.join(' ');
+// console.log(newStr);
+
+
+
+
+
+// let array1 = [1, 2, 3];
+// let array2 = [4, 5, 6];
+// let newArr = array1.concat(array2);
+// console.log(newArr);
+// newArr.reverse();
+// console.log(newArr);
+
+
+// let arr = [9, 6, 3, 7, 2];
+//  arr.sort();
+// console.log(arr);
+// const indexNumber = arr.indexOf(9);
+// console.log(indexNumber);
+
+// let names = ['Haseeb' , 'Adil' , 'Raseed' , 'Khizran']
+// names.push('Zamin')
+// names.pop()
+// names.shift()
+// names.unshift('jhon')
+// let surnames = names.slice(1 , 3)
+// names.splice(1 , 3 , 'bob' )
+// console.log(surnames);
+// console.log(names);
+
+
+
+// let studentNames = ["Alice", "Bob" , "Charlie" , "David" , "Eve"]
+// studentNames.splice(3 ,0 , 'Frank')
+// studentNames.splice(studentNames.indexOf("David"), 1);
+// studentNames.push('Grace' , 'Halen')
+// studentNames.splice(1 , 1 ,'John')
+// console.log(studentNames);
+
+
+
